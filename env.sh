5a835c36ce70e0f8a026183e9a825e34584b8532
Emilio Silva Schlenker <e18r@disroot.org>
2020-12-11 10:56:41 -0500

    get maker contracts from changelog and store them in env vars

---
+++ env.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ -z "$1" ]; then
+    chain=kovan
+else
+    chain=$1
+fi
+echo $chain
+json=$(curl https://changelog.makerdao.com/releases/${chain}/active/contracts.json 2> /dev/null)
+if [[ $json == {* ]]
+   then
+       lines=$(echo $json | sed 's/{ "/export~/' | sed 's/": "/=/g' | sed 's/", "/ export~/g' | sed 's/" }//')
+       for line in $lines; do
+           export=$(echo $line | sed 's/~/ /')
+           echo $export
+           eval $export
+       done
+    else
+        echo "${json}"
+        echo "error"
+   fi
